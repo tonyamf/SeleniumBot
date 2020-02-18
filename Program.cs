d58afabcaf4e89814213284dfb698b987c45ed0a
antonio.franco <antonio.franco@tula-labs.com>
2020-02-18 06:32:16 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Selenium;
+using System.Selenium.Chrome;
+
+
+
+namespace SeleniumBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
